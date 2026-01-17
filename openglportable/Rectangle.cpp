a9fb188f7c1edd75b2cfd8e3cccc1b71dc6b0690
SadiaFardeen <sadiafardeenmahmood@icloud.com>
2026-01-17 12:28:54 +0600

    implemented rect

---
+++ openglportable/Rectangle.cpp
@@ -0,0 +1,17 @@
+#include <GL/glut.h>
+
+void DrawRectangle() {
+    glBegin(GL_POLYGON);
+        glColor3f(0, 0, 1);
+        glVertex2f(0.5, -0.5);
+
+        glColor3f(0, 1, 0);
+        glVertex2f(-0.5, -0.5);
+
+        glColor3f(0, 1, 1);
+        glVertex2f(-0.5, 0.5);
+
+        glColor3f(0, 1, 1);
+        glVertex2f(0.5, 0.5);
+    glEnd();
+}
--- openglportable/Rectangle.h
+++ openglportable/Rectangle.h
@@ -1,6 +1,6 @@
 #ifndef RECTANGLE_H_INCLUDED
 #define RECTANGLE_H_INCLUDED
 
-
+void DrawRectangle();
 
 #endif // RECTANGLE_H_INCLUDED
