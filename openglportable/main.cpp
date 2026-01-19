edff8104b2f60d835a11dc00ccbb7d3d3bb9e907
SadiaFardeen <sadiafardeenmahmood@icloud.com>
2026-01-19 13:22:39 +0600

    Sadia Added Coins File

--- openglportable/main.cpp
+++ openglportable/main.cpp
@@ -1,29 +1,32 @@
 #include <GL/glut.h>
 #include "Environment.h"
+#include "Coins.h"
 
-void display()
-{
+void display() {
     glClear(GL_COLOR_BUFFER_BIT);
 
     drawEnvironment();
+    drawCoins();
 
     glutSwapBuffers();
 }
 
 void timer(int value) {
     updateEnvironment();
+    updateCoins();
+
     glutPostRedisplay();
     glutTimerFunc(16, timer, 0);
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(640, 360);
     glutCreateWindow("PixeDust");
 
     initEnvironment();
+    initCoins();
 
     glutDisplayFunc(display);
     glutTimerFunc(0, timer, 0);
