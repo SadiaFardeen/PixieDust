1a5350329b463cd11fe6238e19362be97a2e538e
SadiaFardeen <sadiafardeenmahmood@icloud.com>
2026-01-19 13:12:08 +0600

    Sadia Added Environment file

--- openglportable/main.cpp
+++ openglportable/main.cpp
@@ -1,21 +1,33 @@
 #include <GL/glut.h>
-#include "Triangle.h"
+#include "Environment.h"
 
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
 
-    DrawTriangle();
+    drawEnvironment();
 
-    glFlush();
+    glutSwapBuffers();
+}
+
+void timer(int value) {
+    updateEnvironment();
+    glutPostRedisplay();
+    glutTimerFunc(16, timer, 0);
 }
 
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(640, 360);
-    glutCreateWindow("OpenGL 64bit");
+    glutCreateWindow("PixeDust");
+
+    initEnvironment();
+
     glutDisplayFunc(display);
+    glutTimerFunc(0, timer, 0);
+
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glutMainLoop();
     return 0;
